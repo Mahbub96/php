a8c1887a904418dc6b9f6d8c683dd02e8749dce9
mahbub96 <mahbub.alam.sobuz@gmail.com>
2022-05-23 01:12:36 +0600

    developing complete a demo website for vip assignment

--- Form.php
+++ Form.php
@@ -1,8 +1,24 @@
 <?php
+
+
         include("Header.php");
         include("Navbar.php");
-        
+        $file_location = "./csv/students.csv";
         $values = array();
+              
+
+        function alertDiv($msg){
+?>
+          <div class="alert alert-warning alert-dismissible fade show text-center" role="alert">
+            <strong class="text-center"><?php echo $msg; ?></strong> 
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+
+<?php
+        }
+        
         if(isset($_POST["submit"])){
 
           // filtering submit buttons values 
@@ -26,19 +42,31 @@
             }
           }
 
+          if(!file_exists($file_location)){
+            $file = fopen($file_location,"a");
+            $table_headers = array("ID","Name","Email","Gender","Batch","Courses","DEPT");
+            fputcsv($file,$table_headers);
+          }
+          else{
+            $file = fopen($file_location,"a");
+          }
           
-          $file = fopen("./csv/students.csv","a");
+
           if(!$file){
             echo "<script>alert('File Opening Failed!')</script>";
             exit(1);
           }
 
           fputcsv($file,$values);
-                  
           fclose($file);
+          alertDiv("Success ! ");
+
         }
 ?>
+
       <div class="container">
+      
+      
         <div class="row">
           <div class="com-md-12">
               <h2 style="text-align:center">Student Information Form</h2>
@@ -64,10 +92,10 @@
             <!-- gender  -->
             <div class="form-group my-2">
               <label>Gender</label>
-              <input type="radio" class="form-check-input" name="gender" id="male" value="male"/>
+              <input type="radio" class="form-check-input" name="gender" id="male" value="male" required/>
               <label for="male" class="form-check-label">Male</label>
 
-              <input type="radio" class="form-check-input" name="gender" id="female" value="female"/>
+              <input type="radio" class="form-check-input" name="gender" id="female" value="female" required/>
               <label for="female" class="form-check-label">Female</label>
 
             </div>
@@ -75,20 +103,20 @@
             <!-- Batch  -->
             <div class="form-group my-2">
               <label>Batch</label>
-              <input type="radio" class="form-check-input" name="batch" id="a" value="69-a"/>
+              <input type="radio" class="form-check-input" name="batch" id="a" value="69-a" required/>
               <label for="a" class="form-check-label">69-A</label>
 
-              <input type="radio" class="form-check-input" name="batch" id="b" value="69-b"/>
+              <input type="radio" class="form-check-input" name="batch" id="b" value="69-b"required/>
               <label for="b" class="form-check-label">69-B</label>
 
-              <input type="radio" class="form-check-input" name="batch" id="c" value="69-c"/>
+              <input type="radio" class="form-check-input" name="batch" id="c" value="69-c"required/>
               <label for="c" class="form-check-label">69-C</label>
 
             </div>
 
             <div class="form-group my-2">
               <label>Courses:</label>
-              <input type="checkbox" class="form-check-input" name="course[0]" id="vip" value="vip"/>
+              <input type="checkbox" class="form-check-input" name="course[0]" id="vip" value="vip" />
               <label for="vip" class="form-check-label">VIP</label>
 
               <input type="checkbox" class="form-check-input" name="course[1]" id="ml" value="ml"/>
@@ -119,6 +147,7 @@
         </div>
         
     </div>
+      </div>
     <script>
     if ( window.history.replaceState ) {
         window.history.replaceState( null, null, window.location.href );
--- index.php
+++ index.php
@@ -3,6 +3,4 @@
   include("Header.php");
   include("Navbar.php");
   include("View.php");
-
-
   include("Footer.php");
